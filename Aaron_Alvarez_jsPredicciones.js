837cfc6e5c7c8a84086084fe170e9a45322ccdb4
qmfs12 <aaron.alvarez.munoz@alumnos.sip.cl>
2022-04-07 11:30:40 -0400

    hola

---
+++ Aaron_Alvarez_jsPredicciones.js
@@ -0,0 +1,18 @@
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}
+// Console.log indica Naci en 1980 //
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+// console.log indica el año en que naci aplicado a la funcion myBirthYearFunc //
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+// console.log esta indicando la suma de num1 + num2 //
