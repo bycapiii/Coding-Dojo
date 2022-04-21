5750a56b0ea66093b929ab5e3311a9de1bb1e60e
qmfs12 <aaron.alvarez.munoz@alumnos.sip.cl>
2022-04-21 09:16:24 -0400

    a

---
+++ Aaron_Alvarez_estavezsi.js
@@ -0,0 +1,8 @@
+function muestraSiElNiñoPuedeSubirALaMontañaRusa(alturaniño) {
+    if (alturaniño + 52) {
+        console.log("!Subete, chico!");
+    }
+else {
+    console.log("Lo siento, chico. Tal vez el próximo año");
+}
+}
